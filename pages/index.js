fb3975ead2c57a845399b2140381f586646e29fa
victor ribero <victor.ribero3@gmail.com>
2019-03-26 11:26:21 +0100

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,14 @@
-import React from 'react'
-import Page from '../src/components/Page'
-import PagePreview from '../src/components/PagePreview'
-import { formatDate } from '../src/utils/date'
-import { makeUrl, filterPosts } from '../src/utils/content'
+import Link from 'next/link'
 
-import CONFIG from '../content/index.json'
-import SUMMARY_JSON from '../content/summary.json'
-
-function Index(props) {
+export default function Index() {
   return (
-    <div>
-      <Page
-        siteTitle={`${CONFIG.siteTitle} - Index`}
-        heroTitle={CONFIG.siteTitle}
-        description={CONFIG.description}
-        stylesheets={CONFIG.stylesheets}
-        topLinks={CONFIG.topLinks}
-        backgroundClass={CONFIG.backgroundClass}
-        body={Body({ summaryJson: SUMMARY_JSON })}
-        copyright={CONFIG.copyright}
-        siteId={CONFIG.siteId}
-      />
-    </div>
+  <div>
+    <Link href={`/blog`}>
+      <a>
+        go to blog
+      </a>
+    </Link>
+    <div>index</div>
+  </div>
   )
 }
-
-function Body(props) {
-  const postList = filterPosts(props.summaryJson)
-  return (
-    <div className="center mw6 pa3 pa4-ns">
-      {postList.map((article, i) => {
-        const href = makeUrl(article)
-        const date = formatDate(article.date)
-        return (
-          <PagePreview
-            title={article.title}
-            preview={article.preview}
-            date={date}
-            href={href}
-            key={i}
-          />
-        )
-      })}
-    </div>
-  )
-}
-
-export default Index
